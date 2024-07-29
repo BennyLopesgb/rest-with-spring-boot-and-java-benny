6ab5c820ca83f4e309e3cedb5a8af0381bf36f32
Benigno Lopes <benigno.lopes@digitro.com>
2024-07-29 00:15:41 -0300

    First Steps in Java with Spring-Boot 3

---
+++ 05_fisrtStepWithJavawithStrpingBoot/rest-with-spring-boot-and-java-benny/src/test/java/br/com/benny/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.benny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
